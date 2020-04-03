4a0da1ad155b11f00f5e5514a538b4d939903904
TimoBrandt1 <41124729+TimoBrandt1@users.noreply.github.com>
2020-04-03 13:41:00 +0200

    State opdracht

---
+++ Collections/Assets/Scenes/AmmoManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoManager : MonoBehaviour
+{
+    Dictionary<string, int> ammoTypes = new Dictionary<string, int>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        ammoTypes.Add("glock", 13);
+        ammoTypes.Add("ak47", 30);
+        ammoTypes.Add("awp", 10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            ammoTypes["glock"] -= 1;
+            Debug.Log(ammoTypes["glock"]);
+        }
+    }
+}
---
+++ Collections/Assets/Scenes/AmmoMangerEnum.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoMangerEnum : MonoBehaviour
+{
+    Dictionary<Weapons, int> ammoTypes = new Dictionary<Weapons, int>();
+    
+    enum Weapons
+    {
+        GLOCK,
+        AK47,
+        AWP
+    }
+    void Start()
+    {
+        ammoTypes.Add(Weapons.GLOCK, 13);
+        ammoTypes.Add(Weapons.AK47, 30);
+        ammoTypes.Add(Weapons.AWP, 10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            ammoTypes[Weapons.GLOCK] -= 1;
+            Debug.Log(ammoTypes[Weapons.GLOCK]);
+        }
+    }
+}
